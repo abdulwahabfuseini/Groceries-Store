b4235c606c955c970587b62a0e0bbbb2aad3621a
fuseini abdul wahab <abdulwahabfuseini78@gmail.com>
2024-01-25 02:20:21 +0000

    Fifth Commit

--- src/utils/formatCurrency.ts
+++
@@ -1,7 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-  currency: "GHC",
-});
-
-export const formatCurrency = (number: number) => {
-  return CURRENCY_FORMATTER.format.number;
-};
